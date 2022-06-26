61a237bba4034f8f79e927a649b4ce122cc07144
oluchioraekwe <oluchi.oraekwe@gmail.com>
2022-06-26 20:44:27 +0100

    editdb

--- dbConnect/dbconnection.js
+++ dbConnect/dbconnection.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
 const winston = require('winston')
-const config = require('config')
+require('dotenv').config()
 
+const db={
+    test:"mongodb://localhost:27017/genres_test",
+    local:"mongodb://localhost:27017/genres",
+    remote:`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.vvnhcpc.mongodb.net/?retryWrites=true&w=majority`
+}
+let dbUri;
+if(process.env.NODE_ENV === "production"){
+    dbUri=db.remote
+}else if(process.env.NODE_ENV === "test"){
+    dbUri=db.test
+}else{
+    dbUri=db.local
+}
 module.exports = ()=>{
-mongoose.connect(config.get('db'))
-.then(()=> winston.info(`Connected to ${config.get('db')}`))
+mongoose.connect(dbUri)
+.then(()=> winston.info(`Connected to ${dbUri}`))
 .catch((error)=>winston.warn('Could not connect to MongoDB...',error.message))
 }
 
